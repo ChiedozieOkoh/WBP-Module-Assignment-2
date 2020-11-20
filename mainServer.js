ced2385c13bbf1228957bcaef2373e57e4453fa0
ChiedozieOkoh <chiedozieokoh1@gmail.com>
2020-11-20 18:19:41 +0000

    Added JSON serialisation
    added functionality to support serialsation of form data in POST requests. Requests are stored as a JSON
    TO-DO: sanitise and validate input from forms

--- mainServer.js
+++ mainServer.js
@@ -1,28 +1,107 @@
 const express = require('express');
-const parser = require('body-parser');
-
+const fs = require('graceful-fs');
 const client = express();
-const jsonParser = parser.json();
+const bodyParser = require('body-parser');
 
+var ReadWriteLock = require('rwlock');
+var lock = new ReadWriteLock();
+client.use(bodyParser.urlencoded({ extended: true }));
+client.use(bodyParser.json());
 client.use(express.static('publicRep'));
+
+
+
 client.get('/',function(req,res){
 console.log("client on homepage");
 });
+
 client.get('/:foo/book',function (req , res){
   //user is requesting a form
   const name = req.params.foo;
   res.send('oh '+ name + ' we got your get request');
 });
-client.post('/contact.html',jsonParser,function(req,res){
+
+class Form{
+    constructor(fName , lName , pNumber,houseNumber,postCode,usrName,age,email,password){
+      this.fName = fName;
+      this.lName = lName;
+      this.pNumber = pNumber;
+      this.houseNumber = houseNumber;
+      this.postCode = postCode;
+      this.usrName = usrName;
+      this.age = age;
+      this.email = email;
+      this.password = password;
+
+    }
+}
+try{
+let file = fs.readFileSync('database.JSON','utf8');
+var jsonList = JSON.parse(file);
+}catch(err){
+  console.log(err);
+}
+
+// check to see if a value is already present in the database
+function isInDatabase(email){
+  //block execution until whole file is read
+
+}
+function addToDatabase(jsonObject){
+  try{
+    fs.appendFileSync('database.JSON',jsonBody + '&');
+    return true;
+  }catch (err){
+    return false;
+  }
+
+}
+function serObject(obj , file){
+  try {
+    fs.writeFileSync(file,JSON.stringify(obj));
+  }catch(err){
+    console.log(err);
+  }
+}
+
+client.post('/booking-signup',function(req,res){
   //user wants to submit form data
    console.log('got a form submission');
-   console.log(req.params);
-   const body = req.body;
-   const name = body.firstName;
-   const email = body.email;
+  responce = "";
 
    res.header('Access-Control-Allow-Origin','*');
-   res.send("Server res : Nice name " + name);
+//   input = jsonParser.parse(req.body);
+
+  // if (isInDatabase(req.body.email)){
+  //   responce = "FAIL: user already registered"
+  // }else{
+  //   if (addToDatabase(req.body) === true ){
+  //     responce = "SUCCESS: user registered succesfully"
+  //   }else{
+  //     responce = "FAIL:Server currently unavailable"
+  //   }
+  // }
+  let newEntry = new Form(req.body.fName,
+    req.body.lName,
+    req.body.phoneNum,
+    req.body.houseNo,
+    req.body.postCode,
+    req.body.usrName,
+    req.body.age,
+    req.body.email,
+    req.body.pwd,
+  );
+
+  console.log(req.body);
+  console.log("in JSON form");
+  console.log(JSON.stringify(req.body));
+  console.log("obj created");
+  console.log(JSON.stringify(newEntry));
+  jsonList.push(newEntry);
+  serObject(jsonList , 'database.JSON');
+  //console.log("first name : "+req.body.fName );
+  //console.log(isInDatabase(req.body.email));
+   res.send(responce);
 });
 
 client.listen(3000,function(){
---
+++ publicRep/wp.js
@@ -0,0 +1,7 @@
+function addLogin_form(){
+    const form_login = document.getElementById("form_login").style.display="block";
+}
+function addSign_up_form(){
+    const form_sign_up = document.getElementById("form_sign-up").style.display="block";
+    const remove_form_login = document.getElementById("form_login").style.display="none";
+}
