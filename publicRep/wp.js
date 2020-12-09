a878e2c38205b2aed53625b9dc89f7b1f1d4c960
caleb <66700303+caleb-adeyemo@users.noreply.github.com>
2020-12-09 09:47:45 +0000

    Made CSS Responsive

--- publicRep/wp.js
+++ publicRep/wp.js
@@ -1,7 +1,12 @@
-function addLogin_form(){
-    const form_login = document.getElementById("form_login").style.display="block";
-}
+// Add sign up form pop up
 function addSign_up_form(){
     const form_sign_up = document.getElementById("form_sign-up").style.display="block";
-    const remove_form_login = document.getElementById("form_login").style.display="none";
+}
+
+function validate_form(){
+    if (document.forms[0].email.value===''){
+        alert("Please Provide An Email Address");
+        return false;
+    }//End if statement
+    return true;
 }
