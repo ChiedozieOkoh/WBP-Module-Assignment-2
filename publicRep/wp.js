5726108a68e7e964a6c2afb65865d86660253424
caleb <66700303+caleb-adeyemo@users.noreply.github.com>
2020-12-10 19:33:47 +0000

    added team.html and form.html

--- publicRep/wp.js
+++ publicRep/wp.js
@@ -1,7 +1,7 @@
 // Add sign up form pop up
-function addSign_up_form(){
+/*function addSign_up_form(){
     const form_sign_up = document.getElementById("form_sign-up").style.display="block";
-}
+}*/
 
 function validate_form(){
     if (document.forms[0].email.value===''){
@@ -9,4 +9,14 @@ function validate_form(){
         return false;
     }//End if statement
     return true;
+}
+function change_background(){
+    if (document.getElementById("test").style.backgroundColor === "grey"){
+        document.getElementsByClassName("info").style.backgroundColor="#333537"
+        console.log("works")
+    }
+    if (document.getElementsByClassName("info").style.backgroundColor === "#333537"){
+        document.getElementsByClassName("info").style.backgroundColor="grey"
+        console.log("also works")
+    }
 }
