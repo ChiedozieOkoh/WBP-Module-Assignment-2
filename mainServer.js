3f568102d44314f279c2a25c297085d8ff4cff9f
ChiedozieOkoh <chiedozieokoh1@gmail.com>
2020-12-10 22:23:03 +0000

    server stores all form elements and have error handling for all fields

--- mainServer.js
+++ mainServer.js
@@ -19,7 +19,7 @@ const ALPHA_NUMERIC = 2;
 
 
 class Form{
-    constructor(fName , lName , pNumber,houseNumber,postCode,usrName,age,email,ethnicity,password){
+    constructor(fName , lName , pNumber,houseNumber,postCode,usrName,age,email,ethnicity,password,fA,sA,weeks,day,gender){
       this.fName = fName;
       this.lName = lName;
       this.pNumber = pNumber;
@@ -30,7 +30,11 @@ class Form{
       this.email = email;
       this.ethnicity = ethnicity;
       this.password = password;
-
+      this.firAppointment = fA;
+      this.secAppointment = sA;
+      this.weeks = weeks;
+      this.day = day;
+      this.gender = gender;
     }
 }
 
@@ -120,7 +124,7 @@ function createErrorMsg(responceObj ,field , error){
      responceObj.status(400);
      responceObj.write('<html>');
      responceObj.write('<body>');
-     responceObj.write('<h1>Error in ' + field +" "+ error + '</h1>');
+     responceObj.write('<h1>Error in ' + field +":  "+ error + '</h1>');
      responceObj.write('</body>');
      responceObj.write('</html>');
      responceObj.end();
@@ -157,7 +161,67 @@ client.get('/:foo/book',function (req , res){
 });
 
 
+function assignTimeSlots(requestObject , entryObject){
+  errorMsg = "NO_ERROR";
+  fA = requestObject.body.firstAm;
+  fP = requestObject.body.firstPm;
 
+  sA = requestObject.body.secondAm;
+  sP = requestObject.body.secondPm;
+  // check for multiple appointments on the same day
+  if ((fA !=null && fP != null) || (sA !=null && sP != null)){
+    errorMsg = "You may only have one appointment per day";
+
+    return errorMsg;
+
+  }
+
+  if ((fA == null && fP == null)||(sA == null && sP == null)){
+    errorMsg = "You must book two appointments one week apart";
+  }
+
+
+
+  list = [fA,fP,sA,sP];
+  for (i = 0 ; i < 4 ; i++){
+    if (list[i] != null ){
+        if(i <2){
+          entryObject.firAppointment = list[i];
+          console.log("First appointment: " + entryObject.firAppointment);
+        }else{
+          console.log(list[i]);
+          entryObject.secAppointment = list[i];
+          console.log("Second appointment: " + entryObject.secAppointment);
+        }
+    }
+  }
+  return errorMsg;
+}
+function assignGender(entryObject,male,female,nBinary){
+  errorMsg = "NO_ERROR";
+  genders = [male,female,nBinary];
+  nActivated = 0;
+
+  if (male!=null){
+    entryObject.gender = "male";
+  }
+  if(female!=null){
+    entryObject.gender = "female";
+  }
+  if(nBinary!=null){
+    entryObject.gender = "non binary";
+  }
+  for (i = 0; i< 3 ; i++){
+      if(genders[i] != null){
+        nActivated++;
+      }
+      if (nActivated > 1 ){
+        errorMsg = "more than one gender was selected , please select non binary if you feel you are neither exclusively male or female";
+        return errorMsg;
+      }
+  }
+  return errorMsg;
+}
 
 client.post('/booking-signup',[
   // replaces special characters with their html counterparts
@@ -186,8 +250,10 @@ client.post('/booking-signup',[
  res.set('Content-Type','text/html');
  console.log(req.body.male + ": male button");
  console.log(req.body);
- console.log("gender" + req.body.genderM);
- console.log("ethnicity" + req.body.ethnicity);
+ console.log("male: " + req.body.male);
+ console.log("female: "+ req.body.female);
+ console.log("nonbinary: "+ req.body.nonbinary);
+ console.log("ethnicity: " + req.body.ethnicity);
   errs = validateBodyMember(req.body.fName,TEXT);
   console.log(req.body.fName);
   console.log(errs);
@@ -232,6 +298,7 @@ if (errs != "NO_ERROR"){
   return createErrorMsg(res,"password",errs);
 }
 
+
   let newEntry = new Form(req.body.fName,
 
     req.body.lName,
@@ -242,9 +309,21 @@ if (errs != "NO_ERROR"){
     req.body.age,
     req.body.email,
     req.body.ethnicity,
-    req.body.pwd
+    req.body.pwd,
+    null, // 1st appointment slot
+    null, // 2nd appointment slot
+    req.body.weeks,
+    req.body.day,
+    null //gender
   );
-
+  errs = assignTimeSlots(req,newEntry);
+  if (errs!= "NO_ERROR"){
+    return createErrorMsg(res,"appointments",errs);
+  }
+  errs = assignGender(newEntry,req.body.male,req.body.female,req.body.nonBinary);
+  if(errs != "NO_ERROR"){
+    return createErrorMsg(res,"gender",errs);
+  }
   console.log(req.body);
   console.log("in JSON form");
   console.log(JSON.stringify(req.body));
